e4ef65e19184ccaeb4ed2cc5d5625669ca729446
Ostrzy <uslugi@ostrzy.net>
2011-06-02 16:32:09 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FaceMaps::Application.config.secret_token = '4343b5406bfd63c17038a5fca082fccc961a0a33f39a68bc634f0da5b17c7d8b705479cc36555675e60e33e2c91a7680597f7e64d6992e3026a0040ca159ca19'
