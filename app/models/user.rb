0ab5527b1ac0a6fdbd8773950af8740528965cb9
Ostrzy <uslugi@ostrzy.net>
2011-06-04 19:29:26 +0200

    fixed problem with null picture link

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,14 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :uid, :provider
 
-  def self.find_for_omniauth uid, provider, user_data
+  def self.find_for_omniauth uid, provider, auth
+    user_data = auth['extra']['user_hash']
     User.find_by_provider_and_uid(provider, uid) || User.create do |user|
       user.provider = provider
       user.uid = uid
       user.name = user_data['name']
       user.location = user_data['location']['name']
-      user.picture = user_data['image'].gsub(/square/, 'normal')
+      user.picture = auth['image'].gsub(/square/, 'normal')
     end
   end
 end
