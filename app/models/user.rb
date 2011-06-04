a934d644a3cb096a53ae6ef8a66c621418724a81
Ostrzy <uslugi@ostrzy.net>
2011-06-04 19:11:49 +0200

    make app to look more like it should look

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
       user.uid = uid
       user.name = user_data['name']
       user.location = user_data['location']['name']
-      user.picture = user_data['image']
+      user.picture = user_data['image'].gsub(/square/, 'normal')
     end
   end
 end
