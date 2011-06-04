cb324e319c744e17998e1f6b7f57613c9c6dfbf6
Ostrzy <uslugi@ostrzy.net>
2011-06-04 21:01:26 +0200

    Added map

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,25 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+function initialize() {
+  var options = {
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.HYBRID
+  };
+  var map = new google.maps.Map(document.getElementById('map_canvas'), options);
+  addMarkerFromAddress(map, document.getElementById('location').innerHTML);
+}
+
+function addMarkerFromAddress(map, address) {
+  var geocoder - google.maps.Geocoder();
+  geocoder.geocode( { 'address': address }, function(results, status) {
+    if(status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: map,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert("Nie udało się znaleźć lokalizacji z następujących powodów: " + status);
+    }
+  }
+}
+
+window.onload = initialize;
