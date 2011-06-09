4ae46297ca66d8f3f8fe3fb0ef528bc6fde97b4f
Ostrzy <uslugi@ostrzy.net>
2011-06-09 07:32:11 +0200

    Added notice message after problems with update

--- app/controllers/users/omniauth_callbacks.rb
+++ app/controllers/users/omniauth_callbacks.rb
@@ -1,9 +1,10 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     auth = request.env['omniauth.auth']
-    user = User.find_for_omniauth auth
+    user, up_succ = User.find_for_omniauth auth
 
     if user.persisted?
+      flash[:notice] = I18n.t(:update_failure) unless up_succ
       sign_in_and_redirect user
     else
       render :text => I18n.t(:log_failure)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,16 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :omniauthable
 
-  # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :location
 
   def self.find_for_omniauth auth
     user_data = auth['extra']['user_hash']
-    uid = auth['uid']
-    provider = auth['provider']
-    if user = User.find_by_provider_and_uid(provider, uid)
-      user.update_attributes(:name => user_data['name'], :location => user_data['location']['name'])
-      user
+    if user = User.find_by_provider_and_uid(auth['provider'], auth['uid'])
+      [user, user.update_attributes(:name => user_data['name'], :location => user_data['location']['name'])]
     else
       User.create do |user|
-        user.provider = provider
-        user.uid = uid
+        user.provider = auth['provider']
+        user.uid = auth['uid']
         user.name = user_data['name']
         user.location = user_data['location']['name']
         user.picture = auth['user_info']['image'].gsub(/square/, 'large')
